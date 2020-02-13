d0b9875291f332f5aa2586f4c21639db8a533fb5
R. Dunbar Poor <rdpoor@gmail.com>
2020-02-12 16:28:03 -0800

    Add ACTestA1U

---
+++ ACTestA1U/ACTestA1U/Debug/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+atmel_start.c
+
+driver_isr.c
+
+main.c
+
+src\ac.c
+
+src\clk.c
+
+src\driver_init.c
+
+src\evsys.c
+
+src\osc.c
+
+src\pmic.c
+
+src\protected_io.S
+
+src\sleep.c
+
